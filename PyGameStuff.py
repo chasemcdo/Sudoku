f762bea2d60dcc6757bea3c042753b43a8eb3ed3
BuildTools <chasemcdougall@hotmail.com>
2020-12-27 16:06:25 -0500

    Update PyGameStuff.py

--- PyGameStuff.py
+++ PyGameStuff.py
@@ -1,7 +1,32 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Dec 27 13:02:23 2020
+# Simple pygame program
 
-@author: chase
-"""
+# Import and initialize the pygame library
+import pygame as py
+py.init()
 
+# Set up the drawing window
+screen = py.display.set_mode([500, 500])
+
+class Square(py.sprite.Sprite):
+    def __init__(self, squareNumber):
+        self.location = squareNumber
+        self.surf.set_colorkey((255, 255, 255), py.RLEACCEL)
+
+square1 = Square(1)
+
+
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            running = False
+
+
+
+    # Flip the display
+    py.display.flip()
+
+# Done! Time to quit.
+py.quit()
