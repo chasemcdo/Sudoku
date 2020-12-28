3a6b84f4046536476bcc59fc99598590e31167b7
BuildTools <chasemcdougall@hotmail.com>
2020-12-28 15:42:00 -0500

    Update PyGameStuff.py
    Started Edits for the Pygame
    
    This addition creates a 9x9 grid on the screen.

--- PyGameStuff.py
+++ PyGameStuff.py
@@ -4,15 +4,31 @@
 import pygame as py
 py.init()
 
+
+#Dimensional Variables for each square
+width = 40
+margin = 2
+boardWidth = 380
+
 # Set up the drawing window
-screen = py.display.set_mode([500, 500])
+screen = py.display.set_mode([boardWidth, boardWidth])
+screen.fill([255,255,255])
 
-class Square(py.sprite.Sprite):
-    def __init__(self, squareNumber):
-        self.location = squareNumber
-        self.surf.set_colorkey((255, 255, 255), py.RLEACCEL)
+currentX = float(margin)
+currentY = float(margin)
+for i in range(0,9):
+    for j in range(0,9):
+        py.draw.rect(screen, [0,0,0], py.Rect(currentX, currentY, width, width))
+        currentX += width + margin
+    currentY += width + margin
+    currentX = float(margin)
+# class Square(py.sprite.Sprite):
+#     def __init__(self, squareNumber):
+#         self.location = squareNumber
+#         self.surf.set_colorkey((255, 255, 255), py.RLEACCEL)
 
-square1 = Square(1)
+
+# square1 = Square(1)
 
 
 running = True
@@ -24,7 +40,8 @@ while running:
             running = False
 
 
-
+    # py.draw.circle(screen, (0, 0, 255), (250, 250), margin/2)
+    
     # Flip the display
     py.display.flip()
 
